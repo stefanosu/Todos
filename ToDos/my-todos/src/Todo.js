b43157d45f6c73d84d9b837c81619190ed9a5596
Stefanos <stefanosu422@gmail.com>
2019-12-08 19:23:34 -0500

    updated JS serializer dependencies

--- ToDos/my-todos/src/Todo.js
+++ ToDos/my-todos/src/Todo.js
@@ -1,23 +1,24 @@
 import React from 'react'
 
-const Todo = (props) => {
+  const Todo = (props) => {
 
 
-        const handleTodo = () => {
-        // console.log(props.todo.id);
-        let ID = props.todo.id 
-        props.removeTodo(ID)
-        }
+  const handleTodo = () => {
+  // console.log(props.todo.id);
+    let ID = props.todo.id 
+    props.removeTodo(ID) 
+    }
 
-        console.log(props.removeTodo);        
+  // console.log(props.removeTodo);        
 
-        return ( 
-        <React.Fragment>
-                <div className='toDo-item'>
-                        <p>{props.todo.name}</p> <button onClick={handleTodo}>Delete</button>
-                </div>
-        </React.Fragment> 
-        );
-}
+  return ( 
+  <React.Fragment>
+          <div className='toDo-item'>
+            <p>{props.todo.name}</p> <button onClick={handleTodo}>Delete</button>
+          </div>
+  </React.Fragment> 
+  );
+  }
 
-export default Todo;
+  export default Todo;
+
