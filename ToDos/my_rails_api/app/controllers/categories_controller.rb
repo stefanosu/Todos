8327c2fb7f98a013da80ce75b7944fbc919a63e2
Stefanos <stefanosu422@gmail.com>
2019-12-08 19:32:54 -0500

    added error boundaries component

--- ToDos/my_rails_api/app/controllers/categories_controller.rb
+++ ToDos/my_rails_api/app/controllers/categories_controller.rb
@@ -1,55 +1,55 @@
 class CategoriesController < ApplicationController
     before_action :set_category, only: [:show, :update, :destroy]
 
-    def index 
-        @categories = Category.all 
-        render json: @categories
-    end
+  def index 
+    @categories = Category.all 
+    render json: @categories
+  end
 
 
-    def show 
-        render json: @category
-    end
+  def show 
+      render json: @category
+  end
 
 
-    def create 
-        # byebug 
-        @category = Category.create(category_params)
-        @categories = Category.all
-        if @category.valid?   
-            render json: @categories
-        else 
-            render json: {errors: @category.errors.full_messages}
-        end
-    end
+  def create 
+      # byebug 
+      @category = Category.create(category_params)
+      @categories = Category.all
+      if @category.valid?   
+          render json: @categories
+      else 
+          render json: {errors: @category.errors.full_messages}
+      end
+  end
 
 
-    def update 
-        # category = Category.update(category_params)
-        if @category.update(category_params)
-            render json: @category
-        else 
-            render json: @category.errors, status: :unprocessable_entity
-        end
-    end
+  def update 
+      # category = Category.update(category_params)
+      if @category.update(category_params)
+          render json: @category
+      else 
+          render json: @category.errors, status: :unprocessable_entity
+      end
+  end
 
 
-    def destroy 
-        # byebug
-        @category.destroy
-        @categories = Category.all
-        render json: @categories
+  def destroy 
+      # byebug
+    @category.destroy
+    @categories = Category.all
+    render json: @categories
 
-    end
+  end
 
 
-    private 
-    def set_category 
-        @category = Category.find(params[:id])
-    end
+  private 
+  def set_category 
+      @category = Category.find(params[:id])
+  end
 
-    def category_params 
-        params.require(:category).permit(:title) 
-    end 
+  def category_params 
+      params.require(:category).permit(:title) 
+  end 
 
-end
+  end
--- ToDos/my_rails_api/app/controllers/todos_controller.rb
+++ ToDos/my_rails_api/app/controllers/todos_controller.rb
@@ -1,51 +1,50 @@
 class TodosController < ApplicationController
     before_action :set_todo, only:[:show, :udpate, :destroy]
 
-    def index
-        @todos = Todo.all
-        render json: @todos
-        
+def index
+    @todos = Todo.all
+    render json: @todos
+end
+
+
+def show 
+    render json: @todo
+end
+
+
+def create
+    @todo = Todo.create(todos_params)
+    @categories = Category.all
+    if @todo.valid? 
+        render json: @categories 
+    else
+        render json: {errors: @todos.error_full_messages}
+    end 
+end
+
+
+def update
+    if @todo.update(todos_params)
+        render json: @todo 
+    else 
+        render json: @todo.errors, status: :unprocessable_entity
     end
+end
 
+def destroy 
+    @todo.destroy
+    @categories = Category.all
+    render json: @categories
+end
 
-    def show 
-        render json: @todo
-    end
+private 
 
+def set_todo 
+    @todo = Todo.find(params[:id])
+end
 
-    def create
-        @todo = Todo.create(todos_params)
-        @categories = Category.all
-        if @todo.valid? 
-            render json: @categories 
-        else
-            render json: {errors: @todos.error_full_messages}
-        end 
-    end
-
-    
-    def update
-        if @todo.update(todos_params)
-            render json: @todo 
-        else 
-            render json: @todo.errors, status: :unprocessable_entity
-        end
-    end
-
-    def destroy 
-        @todo.destroy
-        @categories = Category.all
-        render json: @categories
-    end
-
-    private 
-
-    def set_todo 
-        @todo = Todo.find(params[:id])
-    end
-
-    def todos_params
-        params.permit(:name, :category_id)
-    end
+def todos_params
+    params.permit(:name, :category_id)
+end
 
 end
