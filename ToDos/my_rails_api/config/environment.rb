664d9ef187a8b08b465222987cca7d7b6415bb82
Stefanos <stefanosu422@gmail.com>
2020-03-12 13:40:00 -0400

    added tracer to config file

--- ToDos/my_rails_api/config/environment.rb
+++ ToDos/my_rails_api/config/environment.rb
@@ -4,5 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-# Datadog.configure { |c| c.analytics_enabled = false } 
--- ToDos/my_rails_api/config/initializers/datadog_tracer.rb
+++ ToDos/my_rails_api/config/initializers/datadog_tracer.rb
@@ -1,9 +1,9 @@
 # config/initializers/datadog-tracer.rb
 
-# require 'ddtrace'
+
 
 Datadog.configure do |c|
   c.use :rails, service_name: 'my-rails-app'
-  c.analytics_enabled = false 
+  c.analytics_enabled = true 
   c.tracer tags: { 'env' => 'dev' }
 end
