e001762eeb08901412b88cc0a671b36d1a4e376a
Stefanos <stefanosu422@gmail.com>
2020-03-13 13:16:21 -0400

    added trace tags

--- ToDos/my_rails_api/config/initializers/datadog_tracer.rb
+++ ToDos/my_rails_api/config/initializers/datadog_tracer.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 # config/initializers/datadog-tracer.rb
-
-
 
 Datadog.configure do |c|
   c.use :rails, service_name: 'my-rails-app'
-  c.analytics_enabled = true 
+  c.analytics_enabled = true
   c.tracer tags: { 'env' => 'dev' }
 end
+
+
--- ToDos/my_rails_api/config/routes.rb
+++ ToDos/my_rails_api/config/routes.rb
@@ -1,27 +1,27 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  
-# Categories Routes #  
-get '/getCategories', to: 'categories#index'
-post '/newCategories', to: 'categories#create'
-delete '/removeCategories/:id', to: 'categories#destroy'
-patch  '/updateCategories/:id', to: 'categories#update'
+  # Categories Routes #
+  get '/getCategories', to: 'categories#index'
+  post '/newCategories', to: 'categories#create'
+  delete '/removeCategories/:id', to: 'categories#destroy'
+  patch  '/updateCategories/:id', to: 'categories#update'
 
+  # Todos Routes #
+  get '/getTodos', to: 'todos#index'
+  post '/newTodos', to: 'todos#create'
+  delete '/removeTodos/:id', to: 'todos#destroy'
+  patch '/updateTodos/:id', to: 'todos#update'
 
-# Todos Routes #
-get '/getTodos', to: 'todos#index' 
-post '/newTodos', to: 'todos#create' 
-delete '/removeTodos/:id', to: 'todos#destroy'
-patch '/updateTodos/:id', to: 'todos#update'
+  resources :todos
+  resources :categories
 
-resources :todos 
-resources :categories
-
-# get '/getTodos' do
-  # Datadog.tracer.trace('web.request') do |span|
-  #   span.set_tag('http.url', request.path)
-  #   span.set_tag('<todos>', '<val>')
-  # end
-# end
+  get '/getCategories' do
+    Datadog.tracer.trace('web.request') do |span|
+      span.set_tag('http.url', request.path)
+      span.set_tag('<todos>', '<val>')
+    end
+  end
 end
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
